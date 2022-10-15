56a116b4f65aa472209e375350c77297a0bc71df
jyyang3 <jingyanyang93@email.com>
2022-10-15 15:35:13 +0200

    Oct15_1

--- install/fishbot_description/share/fishbot_description/launch/gazebo.launch.py
+++ install/fishbot_description/share/fishbot_description/launch/gazebo.launch.py
@@ -9,14 +9,14 @@ def generate_launch_description():
     robot_name_in_model = 'fishbot'
     package_name = 'fishbot_description'
     urdf_name = "fishbot_gazebo.urdf"
-
     ld = LaunchDescription()
     pkg_share = FindPackageShare(package=package_name).find(package_name) 
     urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
+    gazebo_world_path = os.path.join(pkg_share, 'world/fishbot.world')
 
     # Start Gazebo server
     start_gazebo_cmd = ExecuteProcess(
-        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',gazebo_world_path],
         output='screen')
 
     # Launch the robot
@@ -24,7 +24,20 @@ def generate_launch_description():
         package='gazebo_ros', 
         executable='spawn_entity.py',
         arguments=['-entity', robot_name_in_model,  '-file', urdf_model_path ], output='screen')
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        arguments=[urdf_model_path]
+        )
 
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        arguments=[urdf_model_path]
+        )    
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
     ld.add_action(start_gazebo_cmd)
     ld.add_action(spawn_entity_cmd)
 
